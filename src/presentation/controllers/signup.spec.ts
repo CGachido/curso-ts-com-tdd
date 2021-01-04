1b6812f95d1508abb0d0d4d38666da3dd0617cec
Cassius Souza <cassius.sanches@gmail.com>
2021-01-04 16:10:36 -0300

    refactor: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -34,4 +34,21 @@ describe('SignUp', () => {
       'Missing param: email'
     ))
   })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'anyemail@gmail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error(
+      'Missing param: password'
+    ))
+  })
 })
