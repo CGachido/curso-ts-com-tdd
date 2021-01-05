3fc437364786a1009599d017c1635f16be79ea7d
Cassius Souza <cassius.sanches@gmail.com>
2021-01-05 15:27:10 -0300

    test: ensure EmailValidatorAdapter calss validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -19,4 +19,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalid_email@email.com')
     expect(isValid).toBe(true)
   })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('invalid_email@email.com')
+    expect(isEmailSpy).toHaveBeenLastCalledWith('invalid_email@email.com')
+  })
 })
