85b5d8e3b5792c7b630358ce711746a3e2ceca50
Cassius Souza <cassius.sanches@gmail.com>
2021-01-15 21:38:40 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGOURL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.MONGOURL || 5050
+  port: process.env.MONGOURL || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'c95f990a0d646f06995b81df24b43307'
 }
