c16fed0ac2c9d64b811974e52a1d28701b48a94a
Cassius Souza <cassius.sanches@gmail.com>
2021-01-19 21:41:48 -0300

    refactor: organize main layer folders

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
-import { adapRoute } from '../adapters/express/express-route-adapter'
+import { adapRoute } from '../adapters/express-route-adapter'
 import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
-import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
+import { makeLoginController } from '../factories/controllers/login/login/login-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adapRoute(makeSignUpController()))
