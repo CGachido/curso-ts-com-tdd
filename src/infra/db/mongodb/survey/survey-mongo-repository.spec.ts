e808a84499e6cdc1065da8328c7efb14a822c1e2
Cassius Souza <cassius.sanches@gmail.com>
2021-01-24 08:17:11 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -38,4 +38,30 @@ describe('Survey Mongo Repository', () => {
       expect(survey).toBeTruthy()
     })
   })
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [
+          { image: 'any_image', answer: 'any_answer' },
+          { answer: 'other_answer' }
+        ],
+        date: new Date()
+      }, {
+        question: 'another_question',
+        answers: [
+          { image: 'another_image', answer: 'another_answer' },
+          { answer: 'other_answer' }
+        ],
+        date: new Date()
+      }])
+
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('another_question')
+    })
+  })
 })
