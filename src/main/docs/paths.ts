add64e601632d23654b503548752779f77594921
Cassius Souza <cassius.sanches@gmail.com>
2021-02-06 09:29:25 -0300

    refactor: move doc to separate files

---
+++ src/main/docs/paths.ts
@@ -0,0 +1,9 @@
+import { loginPath, surveyPath, signUpPath, surveyResultPath } from './paths/'
+
+export default {
+  '/login': loginPath,
+  '/signup': signUpPath,
+  '/surveys': surveyPath,
+  '/surveys/{surveyId}/results': surveyResultPath
+
+}
