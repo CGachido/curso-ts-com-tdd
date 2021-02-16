75378f0904fa231b8701950cf18e30977cf00b6b
Cassius Sanches Gachido de Souza <cassius.sanches@hotmail.com>
2021-02-16 06:16:17 -0300

    test: ensure SignUp Mutation returns on account on valid data

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -77,4 +77,28 @@ describe('Login GraphQL', () => {
       expect(res.errors[0].message).toBe('Invalid param: email')
     })
   })
+
+  describe('SignUp Mutation', () => {
+    const signUpMutation = gql`
+      mutation signUp($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!) {
+        signUp(name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation) {
+          accessToken,
+          name
+        }
+      }
+    `
+    test('Should return an Account on valid data', async () => {
+      const { mutate } = createTestClient({ apolloServer })
+      const res: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'Cassius Souza',
+          email: 'cassius.sanches@gmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        }
+      })
+      expect(res.data.signUp.accessToken).toBeTruthy()
+      expect(res.data.signUp.name).toBe('Cassius Souza')
+    })
+  })
 })
