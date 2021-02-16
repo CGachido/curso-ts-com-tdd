9b0deb796d14920de28ba5ba39be50190eede8a2
Cassius Sanches Gachido de Souza <cassius.sanches@hotmail.com>
2021-02-16 06:58:05 -0300

    test: ensure Surveys Query returns Suveys on success

--- tests/main/graphql/helpers.ts
+++ tests/main/graphql/helpers.ts
@@ -5,5 +5,8 @@ import schemaDirectives from '@/main/graphql/directives'
 import { ApolloServer } from 'apollo-server-express'
 
 export const makeApolloServer = (): ApolloServer => new ApolloServer({
-  resolvers, typeDefs, schemaDirectives
+  resolvers,
+  typeDefs,
+  schemaDirectives,
+  context: ({ req }) => ({ req })
 })
